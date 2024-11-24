ee98547a3c1c16f06d4858ab93fc2762e3b50057
Carl Cappellino <ccappell@umd.edu>
2024-11-23 20:40:32 -0500

    Updated tests for bug_pattern_analysis

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,4 @@
-python3 -m unittest test_bug_pattern_analysis.py
-#python3 -m unittest test_contributor_and_assignee_analysis.py
-#python3 -m unittest test_severity_and_impact_analysis.py
-
+#!/usr/bin/bash
 python3 -m coverage run -m unittest discover
 python3 -m coverage report --omit="*test_*,*/lib/python*/*"
+python3 -m coverage xml
--- test_bug_pattern_analysis.py
+++ test_bug_pattern_analysis.py
@@ -1,43 +1,158 @@
 import unittest
+from unittest.mock import patch
 from io import StringIO
-from unittest.mock import patch, Mock
-
-import warnings
-warnings.simplefilter("ignore", category=UserWarning)
-
-import matplotlib as plt
-plt.use("Agg")  # Non-interactive backend 
+import os
+import copy
 
 from features.bug_pattern_analysis import BugPatternsAnalysis
+from data_loader import DataLoader
+from model import Issue
 
 
 class TestBugPatternAnalysis(unittest.TestCase):
 
     def setUp(self):
-        self.analysis = BugPatternsAnalysis()
+        pass
+
+
+    ##########
+    ### Ensure fetch_and_plot() calls analyze_bug_patterns_for_creator() and no bugs are found for an invalid user.
+    ##########
 
     @patch("sys.stdout", new_callable=StringIO)
     def test_user_failure(self, mock_stdout):
         """
         Check that an invalid user prints the error.
         """
-        # Create BugPatternsAnalysis object/user and plot
+        analysis = BugPatternsAnalysis()
         user:str = "NotARealUser"
-        self.analysis.user = user
-        self.analysis.fetch_and_plot()
+        analysis.user = user
+        analysis.fetch_and_plot()
 
         # Ensure printed output has the analysis's failure
-        output = mock_stdout.getvalue()
-        self.assertIn(f"No bug patterns found for creator '{user}'.", output)
+        self.assertIn(f"No bug patterns found for creator '{user}'.", mock_stdout.getvalue())
+
+
+    ##########
+    ### Test the user can be parsed from the environment
+    ##########
 
     @patch("sys.stdout", new_callable=StringIO)
+    def test_environment_user(self, _):
+        user:str = "json:SomeUser"
+        os.environ["user"] = user
+        analysis = BugPatternsAnalysis()
+        self.assertEqual(f"json:{analysis.user}", user)
+
+        # Cleanup
+        os.environ.pop("user", None)
+
+
+    ##########
+    ### Test plots: Ensure functions succeed and show() is called once
+    ##########
+    
+    @patch("sys.stdout", new_callable=StringIO)
     @patch("matplotlib.pyplot.show")
-    def test_plot_once(self, mock_show, _):
+    def test_general_plot(self, mock_show, _):
         """
-        Check show is called only once
+        Check function succeeds and show() is called once
         """
-        self.analysis.fetch_and_plot()
+        analysis = BugPatternsAnalysis()
+        analysis.user = ""
+        analysis.fetch_and_plot()
         mock_show.assert_called_once()
+    
+    @patch("sys.stdout", new_callable=StringIO)
+    @patch("matplotlib.pyplot.show")
+    def test_creator_plot(self, mock_show, _):
+        """
+        Check function succeeds and show() is called once
+        """
+        analysis = BugPatternsAnalysis()
+        analysis.user = "tigerhawkvok"
+        analysis.fetch_and_plot()
+        mock_show.assert_called_once()
+
+
+    ##########
+    ### Empty issues: Ensure functions fail with proper message if an empty issue list is supplied
+    ##########
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_general_no_issues(self, mock_stdout):
+        issues:list = []
+        analysis = BugPatternsAnalysis()
+        analysis.analyze_general_bug_patterns(issues)
+        self.assertIn("No bug patterns found.", mock_stdout.getvalue())
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_no_issues_creator(self, mock_stdout):
+        issues:list = []
+        user:str = "tigerhawkvok"
+        analysis = BugPatternsAnalysis()
+        analysis.user = user
+        analysis.analyze_bug_patterns_for_creator(issues)
+        self.assertIn(f"No bug patterns found for creator '{user}'.", mock_stdout.getvalue())
+    
+
+    ##########
+    ### None issues: Ensure functions fail if an issues list (or title fields) contains an issue with undeclared fields
+    ##########
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_general_none_issue(self, _):
+        issues:list = [Issue()]
+        analysis = BugPatternsAnalysis()
+        try:
+            analysis.analyze_general_bug_patterns(issues)
+        except TypeError as e:
+            self.fail(f"BugPatternsAnalysis().analyze_general_bug_patterns() failed with: {e}")
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_creator_none_issue(self, _):
+        issues:list = [Issue()]
+        analysis = BugPatternsAnalysis()
+        analysis.user = None
+        try:
+            analysis.analyze_bug_patterns_for_creator(issues)
+        except TypeError as e:
+            self.fail(f"BugPatternsAnalysis().analyze_bug_patterns_for_creator() failed with: {e}")
+
+
+    ##########
+    ### None titles: Ensure functions fail if an issues title is None
+    ##########
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_general_none_title(self, _):
+        global_issues = DataLoader().get_issues()
+        issue_index:int = 10
+        issue = copy.deepcopy(global_issues[issue_index])
+        issue.title = None
+
+        issues:list[Issue] = [issue]
+        analysis = BugPatternsAnalysis()
+        try:
+            analysis.analyze_general_bug_patterns(issues)
+        except TypeError as e:
+            self.fail(f"BugPatternsAnalysis().analyze_general_bug_patterns() failed with: {e}")
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_creator_none_title(self, _):
+        global_issues = DataLoader().get_issues()
+        issue_index:int = 20
+        issue = copy.deepcopy(global_issues[issue_index])
+        issue.title = None
+
+        issues:list[Issue] = [issue]
+        analysis = BugPatternsAnalysis()
+        analysis.user = issue.creator
+        try:
+            analysis.analyze_bug_patterns_for_creator(issues)
+        except TypeError as e:
+            self.fail(f"BugPatternsAnalysis().analyze_bug_patterns_for_creator() failed with: {e}")
+
 
 if __name__ == "__main__":
     unittest.main()
