126397a596486b70ace4352e1f256768bc11cf76
Henrique Corte <hcorte@Henriques-MacBook-Pro.local>
2024-11-24 14:07:26 -0500

    Removed changes to analysis file, changed fetch_and_plot to use argument captors. Added testing for fetch and plot with label

--- coverage/test_contributor_and_assignee_analysis.py
+++ coverage/test_contributor_and_assignee_analysis.py
@@ -39,10 +39,12 @@ class TestContributorAndAssigneeAnalysis(unittest.TestCase):
         with self.assertRaises(ValueError):
             analysis.fetch_and_plot()
 
+
     @patch('builtins.input', side_effect=["2", "2"])
-    def test_fetch_and_plot_correctly_calculates_contributor_counts(self, mock_input):
+    @patch.object(ContributorAndAssigneeAnalysis, 'plot_contributors_assignees_and_labels')
+    def test_fetch_and_plot_correctly_calculates_contributor_df(self, mock_plot, mock_input):
         """
-        Test fetch_and_plot method correctly calculates the contributor_counts
+        Test fetch_and_plot method correctly calculates the contributor_counts and sends it as df to plot
         """
         # Setup
         analysis = ContributorAndAssigneeAnalysis()
@@ -51,16 +53,18 @@ class TestContributorAndAssigneeAnalysis(unittest.TestCase):
         # Action
         analysis.fetch_and_plot()
 
-        # Assertions
-        self.assertEqual(analysis.contributor_counts, {
-            'user1': 2,
-            'user2': 1
-        })
+        args, kwargs = mock_plot.call_args
+        contributor_df, assignee_df, label_df, top_contributors_count, top_assignees_count = args
+
+        # Assert
+        pd.testing.assert_frame_equal(contributor_df, pd.DataFrame([('user1', 2), ('user2', 1)], columns=['Contributor', 'Issue Count']))
+
 
     @patch('builtins.input', side_effect=["2", "2"])
-    def test_fetch_and_plot_correctly_calculates_assignee_counts(self, mock_input):
+    @patch.object(ContributorAndAssigneeAnalysis, 'plot_contributors_assignees_and_labels')
+    def test_fetch_and_plot_correctly_calculates_assignee_df(self, mock_plot, mock_input):
         """
-        Test fetch_and_plot method correctly calculates the assignee_counts
+        Test fetch_and_plot method correctly calculates the assignee_counts and sends it as a df to plot
         """
         # Setup
         analysis = ContributorAndAssigneeAnalysis()
@@ -69,15 +73,16 @@ class TestContributorAndAssigneeAnalysis(unittest.TestCase):
         # Action
         analysis.fetch_and_plot()
 
-        # Assertions
-        self.assertEqual(analysis.assignee_counts, {
-            'assignee1': 2,
-            'assignee2': 1,
-            'assignee3': 1,
-        })
+        args, kwargs = mock_plot.call_args
+        contributor_df, assignee_df, label_df, top_contributors_count, top_assignees_count = args
+
+        # Assert
+        pd.testing.assert_frame_equal(assignee_df, pd.DataFrame([('assignee1', 2), ('assignee2', 1), ('assignee3', 1)], columns=['Assignee', 'Issue Count']))
+
 
     @patch('builtins.input', side_effect=["2", "2"])
-    def test_fetch_and_plot_correctly_calculates_label_counts(self, mock_input):
+    @patch.object(ContributorAndAssigneeAnalysis, 'plot_contributors_assignees_and_labels')
+    def test_fetch_and_plot_correctly_calculates_label_df(self, mock_plot, mock_input):
         """
         Test fetch_and_plot method correctly calculates the label_counts
         """
@@ -88,29 +93,12 @@ class TestContributorAndAssigneeAnalysis(unittest.TestCase):
         # Action
         analysis.fetch_and_plot()
 
-        # Assertions
-        self.assertEqual(analysis.label_counts, {
-            'bug': 2,
-            'high-priority': 1,
-            'feature': 1,
-            'low-priority': 1
-        })
+        args, kwargs = mock_plot.call_args
+        contributor_df, assignee_df, label_df, top_contributors_count, top_assignees_count = args
 
-    @patch('builtins.input', side_effect=["2", "2"])
-    @patch.object(ContributorAndAssigneeAnalysis, 'plot_contributors_assignees_and_labels')
-    def test_fetch_and_plot_calls_plot_method(self, mock_plot, mock_input):
-        """
-        Test fetch_and_plot method calls plot method
-        """
-        # Setup
-        analysis = ContributorAndAssigneeAnalysis()
-        analysis.issues = self.mock_issues
+        # Assert
+        pd.testing.assert_frame_equal(label_df, pd.DataFrame([('bug', 2), ('high-priority', 1), ('feature', 1),('low-priority', 1)], columns=['Label', 'Frequency']))
 
-        # Action
-        analysis.fetch_and_plot()
-
-        # Assertions
-        mock_plot.assert_called_once()
 
     # Failing test - since its a bug, contributer analysis is not handling incomplete data properly
     # @patch('builtins.input', side_effect=["2", "2"])
@@ -131,16 +119,187 @@ class TestContributorAndAssigneeAnalysis(unittest.TestCase):
     #     analysis.fetch_and_plot()
 
     #     # Assertions
-    #     self.assertEqual(analysis.contributor_counts, [{'user3': 1}])
-    #     self.assertEqual(analysis.label_counts, [])
-    #     self.assertEqual(analysis.assignees, [])
+    #     mock_plot.assert_not_called()
 
 
-    ## Plot Contibutors, Assignees and Labels Tests
+## fetch_and_plot_with_label Tests
+
+    # Failing test - since its a bug, invalid inputs should be handled
+    # @patch('builtins.input', side_effect=["0", "0"])
+    # def test_fetch_and_plot_with_label_when_user_inputs_zero_should_print_error_msg(self, mock_input):
+    #     """
+    #     Test fetch_and_plot_with_label when user enters 0 contributors and assignees, should handle gracefully
+    #     """
+        
+    #     analysis = ContributorAndAssigneeAnalysis()
+
+    #     analysis.issues = [
+    #         MagicMock(labels=["feature"], creator="user1", assignees=[{"login": "assignee1"}]),
+    #         MagicMock(labels=["enhancement"], creator="user2", assignees=[{"login": "assignee2"}])
+    #     ]
+
+    #    # Assert that a ValueError is thrown
+    #    with self.assertRaises(ValueError):
+    #    analysis.fetch_and_plot_with_label("bug")
+
+    # Failing test - since its a bug, invalid inputs should be handled
+    # @patch('builtins.input', side_effect=["a", ""])
+    # def test_fetch_and_plot_with_label_when_user_inputs_invalid_should_print_error_msg(self, mock_input):
+    #     """
+    #     Test fetch_and_plot_with_label when user enters 0 contributors and assignees, should handle gracefully
+    #     """
+        
+    #     analysis = ContributorAndAssigneeAnalysis()
+
+    #     analysis.issues = [
+    #         MagicMock(labels=["feature"], creator="user1", assignees=[{"login": "assignee1"}]),
+    #         MagicMock(labels=["enhancement"], creator="user2", assignees=[{"login": "assignee2"}])
+    #     ]
+
+    #     # Assert that a ValueError is thrown
+    #     with self.assertRaises(ValueError):
+    #         analysis.fetch_and_plot_with_label("bug")
 
 
+    @patch('builtins.input', side_effect=["2", "2"])
+    def test_fetch_and_plot_with_label_when_label_not_found_should_print_error_msg(self, mock_input):
+        """
+        Test case where label isn't found
+        """
+        
+        analysis = ContributorAndAssigneeAnalysis()
 
+        analysis.issues = [
+            MagicMock(labels=["feature"], creator="user1", assignees=[{"login": "assignee1"}]),
+            MagicMock(labels=["enhancement"], creator="user2", assignees=[{"login": "assignee2"}])
+        ]
+
+        with patch('builtins.print') as mock_print:
+            analysis.fetch_and_plot_with_label("bug")
+            mock_print.assert_called_with("Error: No such label 'bug' found.")
+
+
+    @patch('builtins.input', side_effect=["2", "2"])
+    @patch('matplotlib.pyplot.subplots')
+    @patch.object(ContributorAndAssigneeAnalysis, 'plot_contributors_and_assignees')
+    def test_fetch_and_plot_with_label_correctly_calculates_contributor_df(self, mock_plot, mock_subplots, mock_input):
+        """
+        Test fetch_and_plot_with_label correctly sends the contributor df when calling plot
+        """
+        # Setup
+        analysis = ContributorAndAssigneeAnalysis()
+        analysis.issues = self.mock_issues
+
+        mock_fig, mock_axes = MagicMock(), [MagicMock(), MagicMock()]
+        mock_subplots.return_value = (mock_fig, mock_axes)
+        
+        # Action
+        analysis.fetch_and_plot_with_label("bug")
+
+        args, kwargs = mock_plot.call_args
+        contributor_df, assignee_df, top_contributors_count, top_assignees_count, label = args
+
+        # Assert
+        pd.testing.assert_frame_equal(contributor_df, pd.DataFrame([('user1', 2)], columns=['Contributor', 'Issue Count']))
+        
+
+    @patch('builtins.input', side_effect=["2", "2"])
+    @patch('matplotlib.pyplot.subplots')
+    @patch.object(ContributorAndAssigneeAnalysis, 'plot_contributors_and_assignees')
+    def test_fetch_and_plot_with_label_correctly_calculates_assignee_df(self, mock_plot, mock_subplots, mock_input):
+        """
+        Test fetch_and_plot_with_label correctly sends the assignee df when calling plot
+        """
+        # Setup
+        analysis = ContributorAndAssigneeAnalysis()
+        analysis.issues = self.mock_issues
+
+        mock_fig, mock_axes = MagicMock(), [MagicMock(), MagicMock()]
+        mock_subplots.return_value = (mock_fig, mock_axes)
+        
+        # Action
+        analysis.fetch_and_plot_with_label("bug")
+
+        args, kwargs = mock_plot.call_args
+        contributor_df, assignee_df, top_contributors_count, top_assignees_count, label = args
+
+        # Assert
+        pd.testing.assert_frame_equal(assignee_df, pd.DataFrame([('assignee1', 1), ('assignee2', 1), ('assignee3', 1)], columns=['Assignee', 'Issue Count']))
+
+
+    @patch('builtins.input', side_effect=["2", "2"])
+    @patch('matplotlib.pyplot.subplots')
+    @patch.object(ContributorAndAssigneeAnalysis, 'plot_contributors_and_assignees')
+    def test_fetch_and_plot_with_label_should_send_correct_label_to_plot(self, mock_plot, mock_subplots, mock_input):
+        """
+        Test fetch_and_plot_with_label calls plot with the correct label
+        """
+        # Setup
+        analysis = ContributorAndAssigneeAnalysis()
+        analysis.issues = self.mock_issues
+
+        mock_fig, mock_axes = MagicMock(), [MagicMock(), MagicMock()]
+        mock_subplots.return_value = (mock_fig, mock_axes)
+        
+        # Action
+        analysis.fetch_and_plot_with_label("bug")
+
+        args, kwargs = mock_plot.call_args
+        contributor_df, assignee_df, top_contributors_count, top_assignees_count, label = args
+
+        # Assert
+        self.assertEqual(label, 'bug')
+
+
+    @patch('builtins.input', side_effect=["2", "2"])
+    @patch('matplotlib.pyplot.subplots')
+    @patch.object(ContributorAndAssigneeAnalysis, 'plot_contributors_and_assignees')
+    def test_fetch_and_plot_with_label_calls_plot_with_correct_contributor_count(self, mock_plot, mock_subplots, mock_input):
+        """
+        Test fetch_and_plot_with_label calls plot with the correct label
+        """
+        # Setup
+        analysis = ContributorAndAssigneeAnalysis()
+        analysis.issues = self.mock_issues
+
+        mock_fig, mock_axes = MagicMock(), [MagicMock(), MagicMock()]
+        mock_subplots.return_value = (mock_fig, mock_axes)
+        
+        # Action
+        analysis.fetch_and_plot_with_label("bug")
+
+        args, kwargs = mock_plot.call_args
+        contributor_df, assignee_df, top_contributors_count, top_assignees_count, label = args
+
+        # Assert
+        self.assertEqual(top_contributors_count, 2)
+
+
+    @patch('builtins.input', side_effect=["2", "2"])
+    @patch('matplotlib.pyplot.subplots')
+    @patch.object(ContributorAndAssigneeAnalysis, 'plot_contributors_and_assignees')
+    def test_fetch_and_plot_with_label_calls_plot_with_correct_assignee_count(self, mock_plot, mock_subplots, mock_input):
+        """
+        Test fetch_and_plot_with_label calls plot with the correct label
+        """
+        # Setup
+        analysis = ContributorAndAssigneeAnalysis()
+        analysis.issues = self.mock_issues
+
+        mock_fig, mock_axes = MagicMock(), [MagicMock(), MagicMock()]
+        mock_subplots.return_value = (mock_fig, mock_axes)
+        
+        # Action
+        analysis.fetch_and_plot_with_label("bug")
+
+        args, kwargs = mock_plot.call_args
+        contributor_df, assignee_df, top_contributors_count, top_assignees_count, label = args
+
+        # Assert
+        self.assertEqual(top_assignees_count, 2)
 
 
 if __name__ == '__main__':
     unittest.main()
+
+    
